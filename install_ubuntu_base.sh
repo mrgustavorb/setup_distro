956c5cb139cf7e11575ceeb97c8937e263b98f14
Gustavo Santiago <gustavosantig1@gmail.com>
2017-12-23 10:51:47 -0300

    fix rvm

--- install_ubuntu_base.sh
+++ install_ubuntu_base.sh
@@ -58,9 +58,8 @@ echo
 echo "======================================================================="
 echo "# INSTALANDO RVM"
 echo "========================================================================"
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-\curl -sSL https://get.rvm.io | bash -s stable
+\curl -sSL https://get.rvm.io | bash -s
 source ~/.rvm/scripts/rvm
 rvm requirements
 
